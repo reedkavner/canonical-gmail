a55b1d614b54e9df54ad50d62a2e4b8ce271ace9
Reed Kavner <rik919@gmail.com>
2013-07-31 08:33:45 -0700

    updated comments

--- canonical_gmail.py
+++ canonical_gmail.py
@@ -4,21 +4,22 @@ When evaluating the addresses of incoming mail, Gmail servers...
 - Ignore periods
 - Ignore any text after a plus sign
 
-Additionally, and @gmail.com  @googlemail.com can be used interchangeably.
+Additionally, @gmail.com and @googlemail.com can be used interchangeably.
 
 So...
 
-EXAMPLE+foo@gmail.com == example+bar@gmail.com == exam.ple@googlemail.com
+example@gmail.com == EXAMPLE+foo@gmail.com == example+bar@gmail.com == exam.ple@googlemail.com
 
 All of these variables make it difficult to check a given email address for uniqueness.
 
-This normalize(email) method in this module transforms any email address on a Gmail server to its 
-canonical representation  (E.g., Example.Test+foo@googlemail.com becomes exampletest@gmail.com) 
-so that it can be compared against other canonical addresses.
+The normalize(email) method in canonical_gmail.py transforms any email address on a Gmail server, 
+including Google Apps addresses with custom domains, to its simplest, canonical representation  
+(E.g., exam.ple+foo@googlemail.com becomes example@gmail.com) so that it can be compared 
+against other canonical addresses.
 
 The canonical address should not be stored in lieu of the user's inputed address, as some
-folks use the plus sign "hack" to route their incoming email to particular folders. These
-Gmail power users can be a persnickety bunch. Don't mess up their system.
+folks use these modifications to route their incoming email to particular folders.These Gmail 
+power users can be a persnickety bunch. Don't mess up their system.
 
 DEPENDENCY: https://github.com/rthalley/dnspython (or 'pip install dnspython')
 '''
